306920eeab66d97d5e8995515af63df55fe51a2b
Kayla Brewer <cooper.kaylam@gmail.com>
2020-09-24 22:24:04 -0500

    setting up an ionic service that will talk to our django API

--- frontEnd/bmFront/src/app/app.module.ts
+++ frontEnd/bmFront/src/app/app.module.ts
@@ -9,14 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ApiDjangoService} from './services/api-django.service'
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(), 
+     HttpClientModule,
+     AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ApiDjangoService
   ],
   bootstrap: [AppComponent]
 })
