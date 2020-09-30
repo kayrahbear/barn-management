7a96992eb799df78970c46514cc4ce428158de40
Kayla Brewer <cooper.kaylam@gmail.com>
2020-09-29 21:06:47 -0500

    making changes to the serializers for the new models

--- django-cloudrun/api/serializers.py
+++ django-cloudrun/api/serializers.py
@@ -1,10 +1,25 @@
-from backoffice.models import User, Horse, Lesson
+from backoffice.models import User, Group, GroupMember, Trainer, Horse, Lesson, Turnout, SuppsMeds, Feed
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = "__all__"
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = "__all__"
+
+class GroupMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupMember
+        fields = "__all__"
+
+class TrainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trainer
         fields = "__all__"
 
 
@@ -17,4 +32,19 @@ class HorseSerializer(serializers.ModelSerializer):
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = "__all__"
+
+class TurnoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Turnout
+        fields = "__all__"
+
+class SuppsMedsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SuppsMeds
+        fields = "__all__"
+
+class FeedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feed
         fields = "__all__"
